c8b414899be3468eaf6c8ec6112c3a19bbd2f40a
timothydowd <timdowd1984@gmail.com>
2019-03-12 11:04:36 +0000

    timeConverter test dones

--- spec/timeConverter.spec.js
+++ spec/timeConverter.spec.js
@@ -1,17 +1,12 @@
-const timeConverter = require('../utils/timeConverter');
+const { timeConverter } = require('../utils/timeConverter');
 const { expect } = require('chai');
 
 
-describe('createRef', () => {
-  it('creates a lookup object when passed a person in an array', () => {
-    const input = [{
-      id: 1,
-      name: 'jezza',
-    }];
-    const expected = {
-      jezza: 1,
-    };
-    expect(createRef(input, 'name', 'id')).to.eql(expected);
+describe('timeConverter', () => {
+  it('converts an array containing unix timestamp into knex acceptable date format', () => {
+    const input = [{ created_at: 1289996514171 }];
+    const expected = [{ created_at: new Date('2010-11-17T12:21:54.171Z') }];
+    console.log(timeConverter(input));
+    expect(timeConverter(input)).to.eql(expected);
   });
-  
 });
