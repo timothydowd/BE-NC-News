c7bf7f13716ba6b4b422530c2feee8e5a0382325
timothydowd <timdowd1984@gmail.com>
2019-04-02 15:58:13 +0100

    changed connection.js to process.env.DATABASE_URL instead of DB_URL - trying to overcome heroku issues

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,6 @@
 const knex = require('knex');
 
 const ENV = process.env.NODE_ENV || 'development';
-const completeConfig = ENV === 'production' ? { client: 'pg', connection: process.env.DB_URL } : require('../knexfile');
+const completeConfig = ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } : require('../knexfile');
 
 module.exports = { connection: knex(completeConfig) };
